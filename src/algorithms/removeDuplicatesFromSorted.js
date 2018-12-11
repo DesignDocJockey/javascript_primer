425d61f1f37197445c8cd8419104dc9046090a2a
Henry Wong <HWong@rcanalytics.com>
2018-12-11 15:30:50 -0500

    Impelemented remove Duplicates In Sorted Array

--- src/algorithms/removeDuplicatesFromSorted.js
+++ src/algorithms/removeDuplicatesFromSorted.js
@@ -12,7 +12,7 @@ It doesn't matter what values are set beyond the returned length.
 
 */
 
-var removeDuplicates = function(nums) {
+var removeDuplicatesFromSortedArray = function(nums) {
     //push a end flag
     nums.push(null)
 
@@ -32,5 +32,27 @@ var removeDuplicates = function(nums) {
 };
 
 const array = [0,0,1,1,1,2,2,3,3,4];
-const results = removeDuplicates(array);
+const results = removeDuplicatesFromSortedArray(array);
 console.log(results);
+
+
+const removeDuplicates = (array) => {
+    const length = array.length; 
+    if(length == 0 || length == 1) return length;
+    
+    let i = 1; 
+    for(let j = 1; j < length; j++) 
+    {
+      if(array[j] != array[j-1])
+      {
+        array[i] = array[j]; 
+        i++; 
+      }
+    }
+    if(i<length) array[i]='\0';
+    // return i; 
+  }
+
+  let array2 = [0,0,1,1,1,2,2,3,3,4];
+  removeDuplicates(array2);
+  console.log(array2);
