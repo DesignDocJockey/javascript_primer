a59b54abd3a0432fe879ad2924efc35434e0a298
DesignDocJockey <hwong18@gmail.com>
2018-12-13 00:12:53 -0500

    Merge 2 linksed lists

--- src/data_structures/LinkedList/linkedlist.js
+++ src/data_structures/LinkedList/linkedlist.js
@@ -2,48 +2,91 @@
 
 const Node = require('./node');
 
-module.exports = class LinkedList 
-{
-    constructor(headNode) {
-        this.headNode = headNode;
-        this.tailNode = null;
-    }
+// module.exports = class LinkedList 
+// {
+//     constructor(headNode) {
+//         this.headNode = headNode;
+//         this.tailNode = null;
+//     }
 
-    insertAtEnd(value) 
-    {
-        if(!this.headNode.getNextNode())
-        {
-            this.tailNode = new Node(value);
-            this.headNode.setNextNode(this.tailNode);
-        }
-        else {
-            let start = this.headNode;
-            while(start) {
-                start = start.getNextNode();
-            }
-            start = new Node(value);
-            this.tailNode = start;
-        }
-    }
-}
+//     insertAtEnd(value) 
+//     {
+//         if(!this.headNode.getNextNode())
+//         {
+//             this.tailNode = new Node(value);
+//             this.headNode.setNextNode(this.tailNode);
+//         }
+//         else {
+//             let start = this.headNode;
+//             while(start) {
+//                 start = start.getNextNode();
+//             }
+//             start = new Node(value);
+//             this.tailNode = start;
+//         }
+//     }
+// }
  
+// const headNode = new Node(18, null);
+// const childNodes = 
+//             new Node(12, 
+//                 new Node(9,
+//                     new Node (7, 
+//                         new Node(12, 
+//                             new Node(15, null)))));
 
+// /**** Append nodes to an existing Linked List  ***/                      
+// headNode.setNextNode(childNodes);
 
-const headNode = new Node(18, null);
-const childNodes = 
-            new Node(12, 
-                new Node(9,
-                    new Node (7, 
-                        new Node(12, 
-                            new Node(15, null)))));
-
-/**** Append nodes to an existing Linked List  ***/                      
-headNode.setNextNode(childNodes);
-
-let node = headNode;
-while(node != null) {
-    console.log(node.getValue());
-    node = node.getNextNode();
-}
+// let node = headNode;
+// while(node != null) {
+//     console.log(node.getValue());
+//     node = node.getNextNode();
+// }
 
 //console.log(JSON.stringify(headNode, undefined, 1));
+
+
+const list1 = new Node(7, 
+                new Node(12,
+                    new Node (18, null)));
+
+const list2 = new Node(1, 
+                new Node(4, 
+                    new Node(8,
+                        new Node (13, null))));
+
+let result = [];
+let list1Iterator = list1;
+let list2Iterator = list2;
+
+while (list1Iterator !== null && list2Iterator !== null) 
+{
+    if( (list1Iterator.getValue() < list2Iterator.getValue()) ) 
+    {
+        result.push(list1Iterator.getValue());
+        list1Iterator = list1Iterator.getNextNode();
+    } 
+    else if( (list1Iterator.getValue() > list2Iterator.getValue()) )
+    {
+        result.push(list2Iterator.getValue());
+        list2Iterator = list2Iterator.getNextNode();
+    }
+    else if (list1Iterator.getValue() === list2Iterator.getValue()) {
+        result.push(listIterator1.getValue());
+        list1Iterator = list1Iterator.getNextNode();
+        list2Iterator = list2Iterator.getNextNode();
+    }
+
+    console.log(`list1Iterator... ${JSON.stringify(list1Iterator, undefined, 1)}`);
+    console.log(`list2Iterator... ${JSON.stringify(list2Iterator, undefined, 1)}`);;
+}
+
+console.log(result);
+let newList = new Node(result[0]);
+for(let i = 1; i < result.length; i++) 
+{
+    let tempNode = new Node(result[i]);
+    newList.setNextNode(tempNode);
+}
+console.log(newList);
